dc5ebd6b68a459b82afd9d788869baa53b8219ba
hemalatha <avhemavinoth@gmail.com>
2016-08-25 22:19:59 +0530

    Update and rename mac.java to mac.c

---
+++ mac.c
@@ -0,0 +1,16 @@
+
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+  int sum,i,n;
+  printf("enter the no");
+  scanf("%d",&n);
+  for(i=0;i<=n;i++)
+  {
+    sum=sum+i;
+  }
+    printf("ans is",sum);
+  
+  }
+
